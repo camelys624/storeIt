8775ef743b53d731eecc666c9dde66f22509cee5
camel <camel_yangz@163.com>
2024-11-27 21:58:51 +0800

    init project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="h1">StoreIt - The only storage you need.</h1>
+    </div>
+  );
+}
